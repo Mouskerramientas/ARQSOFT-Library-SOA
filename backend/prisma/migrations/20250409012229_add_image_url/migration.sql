e729852ff1bb6f566fcb9f352f0bda22db0fab9d
DaniV3611 <danivego2015@gmail.com>
2025-04-08 20:23:50 -0500

    feat(prisma): add imageUrl field to Libro model with default value
    This commit introduces a new field `imageUrl` to the `Libro` model in the Prisma schema, providing a default image URL for books. This change enhances the model by allowing books to have associated images, improving the user experience.

---
+++ backend/prisma/migrations/20250409012229_add_image_url/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Libro" ADD COLUMN     "imageUrl" TEXT NOT NULL DEFAULT 'https://marketplace.canva.com/EAFGf9027eM/1/0/1003w/canva-portada-libro-infantil-bosque-ilustrado-azul-P3McSjgOm1I.jpg';
